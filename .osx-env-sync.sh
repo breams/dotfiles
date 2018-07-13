edb4f7dc3c464eb246c49ebb004730a0bcf93ad9
Jeremy Tweddle <jeremy@breams.net>
2018-07-13 12:15:20 -0400

    Remove .exports from list of parsed files as it contains nothing of interest for us.

--- .osx-env-sync.sh
+++ .osx-env-sync.sh
@@ -1,3 +1,3 @@
-grep "^export PATH" "$HOME/.bash_profile" "$HOME/.path" "$HOME/.exports" "$HOME/.extra" 2>/dev/null | while IFS=' =' read ignoreexport envvar ignorevalue; do
-  launchctl setenv ${envvar} ${!envvar}
+grep "^export PATH" "$HOME/.bash_profile" "$HOME/.path" "$HOME/.extra" 2>/dev/null | while IFS=' =' read ignoreexport envvar ignorevalue; do
+  launchctl setenv ${envvar} "${!envvar}"
 done
